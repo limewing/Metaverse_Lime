4fc66906419c6efac97ba75e0624a47615742f04
limewing <58874904+limewing@users.noreply.github.com>
2025-02-19 18:30:56 +0900

    02.19/18:30

---
+++ Assets/Scripts/Stack/TheStack.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheStack : MonoBehaviour
+{
+    //  TODO : 블록을 놓으면 생성됨
+    //  좌우로 왔다갔다 움직여야함
+    //  버튼 입력 시 좌우 움직임 멈추고 아래로 떨어져야함
+    //  블록이 놓였을 때 겹치지 않는 부분 잘라내야함
+    //  겹치는 영역이 너무 작거나 없으면 게임 오버
+    //  
+}
