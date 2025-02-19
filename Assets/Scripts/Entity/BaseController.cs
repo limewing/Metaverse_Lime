950cd09f3fc8bbe2ac60863de25c0f939134c8a1
limewing <58874904+limewing@users.noreply.github.com>
2025-02-19 11:33:56 +0900

    02.19/11:33
    BaseController/위, 아래 바라볼때는 flip로 못하니까 스프라이트 추가

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] private Transform accessoryPivot;
+
+    [SerializeField] private GameObject rightSprite;
+    [SerializeField] private GameObject upSprite;
+    [SerializeField] private GameObject downSprite;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.right;
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Start()
     {
-        
+
     }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movement(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            lookDirection = direction.normalized; // 마지막 이동 방향을 보고있게 함
+        }
+
+        _rigidbody.velocity = direction * 5;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        if (direction == Vector2.zero)  return;
+
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //  보고있는 각도
+
+        // 왼쪽은 오른쪽 flip시켜서 사용할거임
+        rightSprite.SetActive(false);
+        upSprite.SetActive(false);
+        downSprite.SetActive(false);
+
+        if (Mathf.Abs(rotZ) <= 45f) // 오른쪽
+        {
+            rightSprite.SetActive(true);
+            rightSprite.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (Mathf.Abs(rotZ) > 135f) // 왼쪽
+        {
+            rightSprite.SetActive(true);
+            rightSprite.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (rotZ > 45f && rotZ < 135f) // 위쪽
+        {
+            upSprite.SetActive(true);
+        }
+        else if (rotZ < -45f && rotZ > -135f) // 아래쪽
+        {
+            downSprite.SetActive(true);
+        }
+
+        if (accessoryPivot != null)
+        {
+            accessoryPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        }
+    }
+
 }
